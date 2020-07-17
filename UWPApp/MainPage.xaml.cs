3b092df1f8e548cd21175e7fc600c2fa24cef56d
Asrin Dayananda <adayanan@deakin.edu.au>
2020-07-17 17:09:53 +0100

    Speak button added

--- UWPApp/MainPage.xaml.cs
+++ UWPApp/MainPage.xaml.cs
@@ -26,5 +26,14 @@ namespace UWPApp
         {
             this.InitializeComponent();
         }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MediaElement mediaElement = new MediaElement();
+            var synth = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
+            Windows.Media.SpeechSynthesis.SpeechSynthesisStream stream = await synth.SynthesizeTextToStreamAsync("Hello, How are you");
+            mediaElement.SetSource(stream, stream.ContentType);
+            mediaElement.Play();
+        }
     }
 }
